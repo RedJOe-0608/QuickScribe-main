40ba0aa0b20402ac706b0cf0dd693100e2386e31
jyothi <joeswaroopmakena@gmail.com>
2025-02-05 03:36:17 +0530

    backend schema and context completed

--- app/context/NoteContext.tsx
+++ app/context/NoteContext.tsx
@@ -1,4 +1,6 @@
-import React, { createContext, useEffect, useState } from "react";
+"use client"
+
+import React, { createContext, useContext, useEffect, useMemo, useState } from "react";
 import { v4 as uuidV4 } from "uuid"
 
 type NoteContextType = {
@@ -6,9 +8,10 @@ type NoteContextType = {
     notes: Note[]
     tags: Tag[]
     todos: Todo[]
+    currentProject: Project | null
     onCreateProject: (name: string) => Promise<void>
     onDeleteProject: (name: string) => Promise<void>
-    setCurrentProject: (name: string) => Promise<void>
+    setCurrentProject: React.Dispatch<React.SetStateAction<Project | null>>
     onCreateNote: (data: NoteData) => Promise<void>
     onUpdateNote: (id: string, data: NoteData) => Promise<void>
     onDeleteNote: (id: string) => Promise<void>
@@ -21,7 +24,7 @@ type NoteContextType = {
 }
 
 
-const NoteContext = createContext<NoteContextType | undefined>(undefined)
+const AppContext = createContext<NoteContextType | undefined>(undefined)
 
 export function NoteProvider({children} : {children: React.ReactNode}) {
     const [projects,setProjects] = useState<Project[]>([])
@@ -29,7 +32,6 @@ export function NoteProvider({children} : {children: React.ReactNode}) {
     const [tags,setTags] = useState<Tag[]>([])
     const [todos,setTodos] = useState<Todo[]>([])
     const [currentProject, setCurrentProject] = useState<Project | null>(null)
-
 
     const handleSetCurrentProject = async() => {
 
@@ -40,9 +42,26 @@ export function NoteProvider({children} : {children: React.ReactNode}) {
             getItemsByProjectId(TODOS_STORE,currentProject?.id),
         ]) 
 
-        setNotes(notesData)
+        const notes_data = notesData as RawNote[]
+        const todos_data = todosData as RawTodo[]
+
+        const newNotes = notes_data.map(({tagIds,...other}) => {
+            return {
+                ...other,
+                tags: tags.filter(tag => tagIds.includes(tag.id))
+            }
+        })
+
+        const newTodos = todos_data.map(({tagIds,...other}) => {
+            return {
+                ...other,
+                tags: tags.filter(tag => tagIds.includes(tag.id))
+            }
+        })
+
+        setNotes(newNotes)
         setTags(tagsData)
-        setTodos(todosData)
+        setTodos(newTodos)
        } catch (error) {
             console.log(error);
             
@@ -99,4 +118,199 @@ export function NoteProvider({children} : {children: React.ReactNode}) {
         }
     }
 
+    const onCreateNote = async(data: NoteData) => {
+
+        const newNote: RawNote = {
+            id: uuidV4(),
+            project_id: currentProject?.id,
+            title: data.title,
+            markdown: data.markdown,
+            tagIds: data.tags.map(tag => tag.id)
+        }
+
+        try {
+            await addItem(NOTES_STORE,newNote);
+
+            setNotes(prev => [...prev, {
+                id: newNote.id,
+            project_id: newNote?.project_id,
+            title: newNote.title,
+            markdown: newNote.markdown,
+            tags: data.tags
+            }
+            ])
+        } catch (error) {
+            console.error('Error creating note:', error);
+            
+        }
+    }
+
+    const onUpdateNote = async(id:string,{tags,...data}: NoteData) => {
+
+        const updatedNote:RawNote = {
+            ...data, // title and markdown
+            id,
+            project_id: currentProject?.id,
+            tagIds: tags.map(tag => tag.id)
+        }
+
+        const note =  {
+            id,
+            project_id: currentProject?.id,
+            ...data,
+            tags
+        }
+
+        try {
+            await updateItem(NOTES_STORE,updatedNote);
+
+            setNotes(prev => prev.map(p => p.id === id ? note : p))
+        } catch (error) {
+            console.log("error updating note:", error);
+            
+        }
+
+
+    }
+
+    const onDeleteNote = async(id:string) => {
+
+        try {
+            await deleteItem(NOTES_STORE,id)
+            setNotes(prev => prev.filter(p => p.id !== id))
+            
+        } catch (error) {
+            console.error('Error deleting note:', error);
+        }
+    }
+
+    const addTag = async(label: string, priority: string ) => {
+
+        const newTag: Tag = {
+            id: uuidV4(),
+            project_id: currentProject?.id,
+            label,
+            priority
+        }
+
+        try {
+            await addItem(TAGS_STORE,newTag)
+            setTags(prev => [...prev,newTag])
+        } catch (error) {
+            console.error('Error adding tag:', error);
+        }
+    }
+
+    const updateTag = async(id:string, label:string,priority:string) => {
+
+        const updatedTag:Tag = {
+            id,
+            label,
+            priority,
+            project_id: currentProject?.id
+        }
+
+        await updateItem(TAGS_STORE,updatedTag)
+    }
+
+    const deleteTag = async(id:string) => {
+        try {
+            await deleteItem(TAGS_STORE,id)
+        } catch (error) {
+            console.error('Error deleting tag:', error);
+        }
+    }
+
+    const addTodo = async({tags,...data}: TodoData) => {
+
+        const newTodo:RawTodo = {
+            ...data,
+            id:uuidV4(),
+            project_id:currentProject?.id,
+            tagIds: tags.map(tag => tag.id)
+        }
+
+        try {
+            await addItem(TODOS_STORE,newTodo)
+
+            setTodos(prev => [...prev,{
+                ...data,
+                id: newTodo.id,
+                project_id: newTodo.project_id,
+                tags
+            }])
+        } catch (error) {
+            console.log("error adding todos", error);
+        }
+    }
+
+    const updateTodo = async(id:string, {tags,...data}: TodoData) => {
+
+        const updatedTodo: RawTodo = {
+            ...data,
+            id,
+            project_id:currentProject?.id,
+            tagIds: tags.map(tag => tag.id)
+        } 
+
+        const todo: Todo = {
+            ...data,
+            id,
+            project_id:currentProject?.id,
+            tags
+        }
+
+        try {
+            await updateItem(TODOS_STORE,updatedTodo)
+            setTodos(prev => prev.map(p => p.id === id ? todo : p))
+        } catch (error) {
+            console.log("error updating todo", error);
+            
+        }
+    }
+
+    const deleteTodo = async(id:string) => {
+        try {
+            await deleteItem(TODOS_STORE,id)
+            setTodos(prev => prev.filter(p => p.id !== id))
+        } catch (error) {
+            console.log("error deleting todo",error);
+                        
+        }
+    }
+
+
+    return (
+        <AppContext.Provider
+          value={{
+            projects,
+            currentProject,
+            notes,
+            tags,
+            todos,
+            onCreateProject,
+            onDeleteProject,
+            setCurrentProject,
+            onCreateNote,
+            onUpdateNote,
+            onDeleteNote,
+            addTag,
+            updateTag,
+            deleteTag,
+            addTodo,
+            updateTodo,
+            deleteTodo
+          }}
+        >
+          {children}
+        </AppContext.Provider>
+      )
+}
+
+export function useAppContext() {
+    const context = useContext(AppContext);
+    if (context === undefined) {
+        throw new Error("useNoteContext must be used within a NoteProvider");
+      }
+      return context;
 }
--- app/types/types.ts
+++ app/types/types.ts
@@ -5,7 +5,7 @@ type Project = {
 
 type RawNote = {
     id: string
-    project_id: string
+    project_id: string | undefined
     title: string
     markdown: string
     tagIds: string[] // array of tag ids
@@ -13,7 +13,7 @@ type RawNote = {
 
 type Note = {
     id: string
-    project_id: string
+    project_id: string | undefined
     title: string
     markdown: string
     tags: Tag[]
@@ -27,22 +27,33 @@ type NoteData = {
 
 type Tag = {
     id: string
-    project_id: string
+    project_id: string | undefined
     label: string
     priority: string // this will be a hex value
 
 }
 
-type Todo = {
+type RawTodo = {
     id: string
-    project_id: string
+    project_id: string | undefined
     task: string
     isCompleted: boolean
     deadline: string // this is a date.
+    tagIds: string[] // array of tag ids
+}
+
+type Todo = {
+    id: string
+    project_id: string | undefined
+    task: string
+    isCompleted: boolean
+    deadline: string // this is a date.
+    tags: Tag[] // array of tag ids
 }
 
 type TodoData = {
     task: string
     isCompleted: boolean
     deadline: string // this is a date.
+    tags: Tag[] // array of tag ids
 }
